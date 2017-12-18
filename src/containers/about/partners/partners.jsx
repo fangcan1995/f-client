8bdef6a12f3b84c3199a32553868f36b7ed6e248
liuli <liuli@bbh.com>
2017-12-18 16:34:07 +0800

    关于我们

---
+++ src/containers/about/partners/partners.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default ({ location, match, history }) => {
+    return(
+
+        <div>
+            <div className="m-crumb">
+                <div>您所在的位置： 合作伙伴</div>
+            </div>
+
+        </div>
+    )
+}
