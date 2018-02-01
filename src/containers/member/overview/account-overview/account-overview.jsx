bd03f1f8f2e8fa29b076d0fc894adce42c55a903
liuli <liuli@bbh.com>
2018-02-01 09:59:34 +0800

    关于我们

--- src/containers/member/overview/account-overview/account-overview.jsx
+++ src/containers/member/overview/account-overview/account-overview.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import ReactEcharts from 'echarts-for-react';
+import {getEchartPie,getEchartLine,getEchartBar} from '../../../../assets/js/getEchart';
 import './account-overview.less';
 /*import Crumbs from '../../../../components/crumbs/crumbs.jsx';*/
-
+//折线图数据
+let data_Chart1={
+    xAxis_data:['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+    legend:{data:['累计收益1','累计收益2']},
+    series_data:[
+        {
+            name:"累计收益1",
+            type:'line',
+            data:[5.6, 4.9, 89, 23.2, 25.6, 76.7, 6.6, 162.2, 32.6, 20, 5.4, 3.3]
+        }
+    ]
+};
+//柱状图数据
+let data_Chart2={
+    xAxis_data:['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+    legend:{data:['']},
+    series_data:[
+        {
+            name:"",
+            type:'bar',
+            barWidth:30,
+            itemStyle:{
+                normal:{
+                    color:'#668bc1',  //柱体颜色
+                    label: {
+                        show:true,
+                        formatter: "{c}万",
+                        position:'top'
+                    }
+                }
+            },
+            data:[5.6, 4.9, 89, 23.2, 25.6, 76.7, 6.6, 162.2, 32.6, 20, 5.4, 3.3]
+        }
+    ]
+};
+/*饼图数据*/
+let data_Chart3={
+    legend:{data:['月汇宝资产','散标资产','可用余额','冻结金额']},
+    series_data:{
+        data:[
+            {value:10000.00, name:'月汇宝资产'},
+            {value:500.00, name:'散标资产'},
+            {value:780.00, name:'可用余额'},
+            {value:100, name:'冻结金额'}
+        ]
+    }
+}
 export default ({ location, match, history }) => {
   return (
     <div className="member__main">
@@ -93,6 +140,26 @@ export default ({ location, match, history }) => {
                 </div>
                 <div className="incomeForm"></div>
             </div>
+        </div>
+        <div className="incomeState">
+            <ReactEcharts
+                option={getEchartLine(data_Chart1)}
+                style={{height: '300px', width: '100%'}}
+                opts={{renderer: 'svg'}}
+                className='react_for_echarts'
+            />
+            <ReactEcharts
+                option={getEchartBar(data_Chart2)}
+                style={{height: '300px', width: '100%'}}
+                opts={{renderer: 'svg'}}
+                className='react_for_echarts'
+            />
+            <ReactEcharts
+                option={getEchartPie(data_Chart3)}
+                style={{height: '300px', width: '100%'}}
+                opts={{renderer: 'svg'}}
+                className='react_for_echarts'
+            />
         </div>
     </div>
     );
