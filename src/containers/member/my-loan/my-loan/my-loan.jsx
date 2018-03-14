73bf6154d1223c722c79ba4a25f57b9c1b40a453
liuli <liuli@bbh.com>
2018-03-14 18:38:04 +0800

    我的借款

--- src/containers/member/my-loan/my-loan/my-loan.jsx
+++ src/containers/member/my-loan/my-loan/my-loan.jsx
@@ -5,7 +5,6 @@ import {addCommas} from '../../../../assets/js/cost';
 import Crumbs from '../../../../components/crumbs/crumbs';
 import Tab from '../../../../components/tab/tab';
 import Pagination from '../../../../components/pagination/pagination';
-import  {getData}  from '../../../../assets/js/getData';
 import { Modal } from 'antd';
 import ModalRepaymentApp from './modalRepaymentApp';
 import { connect } from 'react-redux';
@@ -13,178 +12,6 @@ import  memberLoansActions  from '../../../../actions/member-loans';
 import moment from "moment";
 import './my-loan.less';
 class MyLoans extends React.Component {
-    /*constructor(props){
-        super(props);
-        this.state={
-            modalRepaymentApp: false,
-            currentId:'',
-            dataSetting:{},  //项目数据
-            charts:{
-                totalInvestment:{},
-                accumulatedIncome:{},
-            },  //统计数据
-            status: 1,
-        };
-    }
-    toggleModal=(modal,visile,id)=>{
-        if(visile){
-            this.setState({
-                [modal]: true,
-                currentId:id,
-            });
-        }else{
-            this.setState({
-                [modal]: false,
-                currentId:'',
-            });
-        }
-    };
-    switchFilterStyle = (item,index) => {
-        return index === this.state[item] ? "filter__opt filter__opt--active" : "filter__opt"
-    }
-    filter=(item,index)=>{
-        this.setState({
-            [item]: index,
-        },()=>{
-            let filter={
-                status:this.state.status,
-            }
-            this.loadData(1,10,filter);
-        })
-    }
-    loadData(currentPage,pageSize,filter){
-        let data=getData(`http://localhost:9002/members`,currentPage,pageSize,filter);
-        if (data){
-            this.setState({
-                dataSetting:data.data
-            });
-        }else{
-            let mockDate={
-                code: "0",
-                data:{
-                    list:[
-                        {
-                            proId:'1',proStatus:'项目状态',shortText:'项目名称',proMoney:'投资总额',proMoneyEnd:'投资金额',
-                            inveCreateTime:'投资时间',proAnnualRate:'预期年化收益率',loanExpiry:'锁定期限',loanRefundWay:'还款方式',
-                            proMoneyPercent:'投资进度',earnShdEarnDate:'下期回款日期',earnShdEarnDate:'下期回款日期',earnShdEarnAmou:'下期回款金额',
-                            earnRemittancAmou:'回款金额',earnRealEarnDate:'结清时间',transNo:'债转标项目名称',transApplyTime:'转让申请日期',
-                            transStatus:'结果',transFinanced:'当前投资额',transSchedule:'债转投资进度',transPutDate:'转让日期',
-                            transferDate:'转让成功日期',transAmt:'转让金额',transFee:'手续费'
-                        },
-                        {
-                            proId:'2',proStatus:'项目状态',shortText:'汇车贷-HCD201704170001',proMoney:'投资总额',proMoneyEnd:'投资金额',
-                            inveCreateTime:'投资时间',proAnnualRate:'预期年化收益率',loanExpiry:'锁定期限',loanRefundWay:'还款方式',
-                            proMoneyPercent:'投资进度',earnShdEarnDate:'下期回款日期',earnShdEarnDate:'下期回款日期',earnShdEarnAmou:'下期回款金额',
-                            earnRemittancAmou:'回款金额',earnRealEarnDate:'结清时间',transNo:'债转标项目名称',transApplyTime:'转让申请日期',
-                            transStatus:'结果',transFinanced:'当前投资额',transSchedule:'债转投资进度',transPutDate:'转让日期',
-                            transferDate:'转让成功日期',transAmt:'转让金额',transFee:'手续费'
-                        },
-                        {
-                            proId:'3',proStatus:'项目状态',shortText:'汇车贷-HCD201704170002',proMoney:'投资总额',proMoneyEnd:'投资金额',
-                            inveCreateTime:'投资时间',proAnnualRate:'预期年化收益率',loanExpiry:'锁定期限',loanRefundWay:'还款方式',
-                            proMoneyPercent:'投资进度',earnShdEarnDate:'下期回款日期',earnShdEarnDate:'下期回款日期',earnShdEarnAmou:'下期回款金额',
-                            earnRemittancAmou:'回款金额',earnRealEarnDate:'结清时间',transNo:'债转标项目名称',transApplyTime:'转让申请日期',
-                            transStatus:'结果',transFinanced:'当前投资额',transSchedule:'债转投资进度',transPutDate:'转让日期',
-                            transferDate:'转让成功日期',transAmt:'转让金额',transFee:'手续费'
-                        },
-                        {
-                            proId:'4',proStatus:'项目状态',shortText:'汇车贷-HCD201704170003',proMoney:'100000.00',proMoneyEnd:'50000.00',
-                            inveCreateTime:'2016-12-12',proAnnualRate:'12%',loanExpiry:'3个月',loanRefundWay:'还款方式',
-                            proMoneyPercent:'投资进度',earnShdEarnDate:'下期回款日期',earnShdEarnDate:'下期回款日期',earnShdEarnAmou:'下期回款金额',
-                            earnRemittancAmou:'回款金额',earnRealEarnDate:'结清时间',transNo:'巴20170426字00061号',transApplyTime:'转让申请日期',
-                            transStatus:'结果',transFinanced:'7000.00',transSchedule:'70%',transPutDate:'2017-06-06',
-                            transferDate:'转让成功日期',transAmt:'10000.00',transFee:'手续费'
-                        },
-                    ],
-                    pageNum: 1,
-                    pageSize: 10,
-                    total:13
-                },
-                message: "SUCCESS",
-                time: "2018-01-17 11:49:39"
-            };
-            this.setState({
-                dataSetting:mockDate.data
-            });
-        }
-
-    }
-    loadChartsData(){
-        let data=getData(`http://localhost:9002/members`);
-        if (data){
-            /!*this.setState({
-                charts:{
-                    totalInvestment:{
-                        legend:{data:['招标中','回款中','已回款','已转出']},
-                        series_data:{
-                            data:[
-                                {value:data.data.totalInvestment.proMoneyBidding, name:'招标中'},
-                                {value:data.data.totalInvestment.proMoneyInBack, name:'回款中'},
-                                {value:data.data.totalInvestment.proMoneyBacked, name:'已回款'},
-                                {value:data.data.totalInvestment.proMoneyOut, name:'已转出'}
-                            ]
-                        }
-                    },
-                    accumulatedIncome:{
-                        legend:{data:['招标中','回款中','已回款','已转出']},
-                        series_data:{
-                            data:[
-                                {value:data.data.totalInvestment.proMoneyBidding, name:'招标中'},
-                                {value:data.data.totalInvestment.proMoneyInBack, name:'回款中'},
-                                {value:data.data.totalInvestment.proMoneyBacked, name:'已回款'},
-                                {value:data.data.totalInvestment.proMoneyOut, name:'已转出'}
-                            ]
-                        }
-                    }
-                }
-            });*!/
-        }else{
-            let mockDate={
-                data: {
-                    totalInvestment: {
-                        proMoneyBidding:5000.00,
-                        proMoneyInBack:2000.00,
-                        proMoneyBacked:3000.00,
-                        proMoneyOut:1000.00,
-                    },  //投资总额
-                    accumulatedIncome: {
-                        earnMoneyInBack:5000.00,
-                        earnMoneyBacked:5000.00,
-                        earnMoneyOut:2000.00,
-                    },  //累计收益
-                },
-                code: "0",
-                message: "SUCCESS",
-            };
-            let {totalInvestment,accumulatedIncome}=mockDate.data;
-            this.setState({
-                charts:{
-                    totalInvestment:{
-                        data:[
-                            {name:'申请中',value:totalInvestment.proMoneyBidding,instruction:`${addCommas(totalInvestment.proMoneyBidding)}元`  },
-                            {name:'招标中',value:totalInvestment.proMoneyInBack,instruction:`${addCommas(totalInvestment.proMoneyInBack)}元`},
-                            {name:'还款中',value:totalInvestment.proMoneyBacked,instruction:`${addCommas(totalInvestment.proMoneyBacked)}元`},
-                            {name:'已结清',value:totalInvestment.proMoneyOut,instruction:`${addCommas(totalInvestment.proMoneyOut)}元`}
-                        ]
-                    },
-                    accumulatedIncome:{
-                            data:[
-                                {name:'还款中',value:accumulatedIncome.earnMoneyInBack,instruction:`${addCommas(accumulatedIncome.earnMoneyInBack)}元`  },
-                                {name:'已结清',value:accumulatedIncome.earnMoneyBacked,instruction:`${addCommas(accumulatedIncome.earnMoneyBacked)}元`  },
-                            ]
-                    },
-                }
-            },()=>{
-
-
-            });
-        }
-    }
-    componentDidMount () {
-        this.loadChartsData();
-        this.loadData(1,10,{status:1});
-
-    }*/
     componentDidMount () {
         this.props.dispatch(memberLoansActions.getData(1));
     }
@@ -285,8 +112,8 @@ class MyLoans extends React.Component {
                                                             <td>--</td>
                                                             <td>{l.shortText}</td>
                                                             <td>{l.money}</td>
-                                                            <td>{l.num}</td>
-                                                            <td>{l.num}</td>
+                                                            <td>{l.num}%</td>
+                                                            <td>{l.num}个月</td>
                                                             <td>{l.shortText}</td>
                                                             <td>{moment(l.dateTime).format('YYYY-MM-DD')}</td>
                                                             <td>申请中</td>
@@ -295,10 +122,10 @@ class MyLoans extends React.Component {
                                                         <tr key={`row-${i}`}>
                                                             <td><p><a href="#">{l.longText}</a></p></td>
                                                             <td>{l.shortText}</td>
-                                                            <td>{l.shortText}</td>
+                                                            <td>{l.shortText}个月</td>
                                                             <td>{moment(l.dateTime).format('YYYY-MM-DD')}</td>
                                                             <td>{l.shortText}</td>
-                                                            <td>{l.shortText}</td>
+                                                            <td>{l.shortText}%</td>
                                                             <td>{moment(l.dateTime).format('YYYY-MM-DD')}</td>
                                                             <td>招标中</td>
                                                         </tr>
@@ -306,7 +133,7 @@ class MyLoans extends React.Component {
                                                         <tr key={`row-${i}`}>
                                                             <td><p><a href="#">{l.longText}</a></p></td>
                                                             <td>{l.money}</td>
-                                                            <td>{l.num}</td>
+                                                            <td>{l.num}个月</td>
                                                             <td>{moment(l.dateTime).format('YYYY-MM-DD')}</td>
                                                             <td>{moment(l.dateTime).format('YYYY-MM-DD')}</td>
                                                             <td>{l.money}</td>
@@ -324,7 +151,7 @@ class MyLoans extends React.Component {
                                                         <tr key={`row-${i}`}>
                                                             <td><p><a href="#">{l.longText}</a></p></td>
                                                             <td>{l.shortText}</td>
-                                                            <td>{l.shortText}</td>
+                                                            <td>{l.shortText}个月</td>
                                                             <td>{moment(l.dateTime).format('YYYY-MM-DD')}</td>
                                                             <td>{l.shortText}</td>
                                                             <td>{l.shortText}</td>
@@ -359,16 +186,11 @@ class MyLoans extends React.Component {
                 <Modal
                     title="提前还款申请"
                     wrapClassName="vertical-center-modal"
-                    //visible={this.state.modalRepaymentApp}
                     visible={modalRepaymentApp}
                     width="520px"
                     footer={null}
-                    //onCancel={() => this.toggleModal(`modalRepaymentApp`,false,'')}
                     onCancel={() => this.repaymentCallback()}
                 >
-                    {/*{this.state.modalRepaymentApp===true?
-                        <ModalRepaymentApp proId={this.state.currentId} />:''
-                    }*/}
                     {modalRepaymentApp===true?
                         <ModalRepaymentApp info={
                             {
@@ -392,5 +214,4 @@ function mapStateToProps(state) {
         memberLoans
     };
 }
-
 export default connect(mapStateToProps)(MyLoans);
