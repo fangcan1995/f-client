346dfa35f92dbd67e12aa0d59b0769c67cab263c
liuli <liuli@bbh.com>
2018-03-20 17:06:31 +0800

    我的资料

--- src/containers/member/settings/my-authInfo/my-authInfo.jsx
+++ src/containers/member/settings/my-authInfo/my-authInfo.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Crumbs from '../../../../components/crumbs/crumbs';
 import Tab from '../../../../components/tab/tab';
+import MyAvatar from '../../../../components/myAvatar/myAdatar';
 import './authInfo.less';
 import { connect } from 'react-redux';
-import {myRiskAssessAc} from '../../../../actions/member-settings';
+import {myAuthInfoAc} from '../../../../actions/member-settings';
 class MyAuthInfo extends React.Component {
+    componentDidMount() {
+        this.props.dispatch(myAuthInfoAc.getResult());
+    }
     render(){
+        console.log(this.props);
+        let {authInfo}=this.props.memberSettings;
         return(
             <div className="member__main">
                 <Crumbs/>
@@ -14,22 +20,24 @@ class MyAuthInfo extends React.Component {
                     <Tab>
                         <div name="基本信息">
                             <table className="authInfo">
+
+                                <tbody>
                                 <tr>
                                     <th><i className="iconfont icon-user"></i>真实姓名</th>
                                     <td className="Result">已认证</td>
-                                    <td className="detail">柳岩 </td>
+                                    <td className="detail">{authInfo.trueName}</td>
                                     <td className="operate">不可更改</td>
                                 </tr>
                                 <tr>
                                     <th><i className="iconfont icon-phone"></i>手机号</th>
                                     <td className="Result">已设置</td>
-                                    <td className="detail">159****4086</td>
+                                    <td className="detail">{authInfo.phoneNumber}</td>
                                     <td className="operate"><a href="javascript:void(0);" className="changePhoneNumber">更改</a></td>
                                 </tr>
                                 <tr>
                                     <th><i className="iconfont icon-id"></i>实名认证</th>
                                     <td className="Result"><span className="importantTxt">已认证</span></td>
-                                    <td className="detail">210***********3621</td>
+                                    <td className="detail">{authInfo.idNumber}</td>
                                     <td className="operate">不可更改</td>
                                 </tr>
                                 <tr className="no">
@@ -46,10 +54,13 @@ class MyAuthInfo extends React.Component {
                                     <td className="detail">******</td>
                                     <td className="operate"><a href="javascript:void(0);" className="login_pass">修改登录密码</a></td>
                                 </tr>
+                                </tbody>
                             </table>
                         </div>
                         <div name="头像设置">
-                            2
+                            <div className="tab_content">
+                                <MyAvatar/>
+                            </div>
                         </div>
                     </Tab>
 
