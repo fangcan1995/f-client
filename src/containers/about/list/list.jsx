6a002f172f96086771dfbd5f80f8ea61687a6ba3
guifeng <fengyunlingying@qq.com>
2018-04-16 14:22:34 +0800

    change ip
    add info

--- src/containers/about/list/list.jsx
+++ src/containers/about/list/list.jsx
@@ -4,59 +4,9 @@ import Pagination from '../../../components/pagination/pagination';
 import Crumbs from '../../../components/crumbs/crumbs';
 import Tab from '../../../components/tab/tab';
 import { Route, Link } from 'react-router-dom';
-import { articalListAction } from '../../../actions/about';
+import { aboutContentAction, articalListAction } from '../../../actions/aboutContent';
 
 
-/* const setTitle = (typeText) => {
-    switch (typeText) {
-        case 'mediaCompany':
-            return '公司动态';
-        case 'mediaReport':
-            return '媒体报道';
-        case 'mediaIndustry':
-            return '行业新闻';
-        case 'notice':
-            return '网站公告';
-        case 'report':
-            return '运营报告';
-        case 'laws':
-            return '相关规则';
-        case 'activeNotice':
-            return '活动公告';
-        case 'questions':
-            return '常见问题';
-        case '56':
-            return '风控流程';
-        case '57':
-            return '风险提示';
-        case '58':
-            return '风险教育';
-    }
-} */
-
-
-/* const articalList = (props) => {
-    console.log(props);
-    const { dispatch, aboutContent } = props;
-    return (
-        <div>
-            123123123213423
-            {<Pagination config={
-                {
-                    currentPage: aboutContent.pageInfo.pageNum,
-                    pageSize: aboutContent.pageInfo.pageSize,
-                    totalPage: aboutContent.pageInfo.pages,
-                    paging: (obj) => {
-                        console.log(obj);
-                        dispatch(articalListAction(obj.currentPage,obj.pageCount))
-                        //this.loadData(obj.currentPage,obj.pageCount);
-                    }
-                }
-            } ></Pagination>}
-        </div>
-    );
-
-} */
 
 
 
@@ -66,53 +16,67 @@ class articalList extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            typeTitle: ''
-        }
+    }
+
+    componentDidMount() {
+        const { tabName, match, content, childId } = this.props;
+        console.log(childId);
+    }
+
+    componentDidUpdate() {
+        const { tabName, match, content, childId, dispatch } = this.props;
+        dispatch(articalListAction(childId));
     }
 
     render() {
 
         //数据
-        console.log(this.props);
-        const { list } = this.props.data;
-
-        const listData = list ? list : [];
-
-        //路由
-        const { match } = this.props;
-        const urlToArray = match.url.split('/');
-        const typeText = urlToArray[urlToArray.length - 1];
-        //const typeTitle = setTitle(typeText);
-
-
+        const { tabName, match, content, childId, dispatch } = this.props;
         return (
-            <ul className="list">
-                {
-                    listData.map((item, i) => {
-                        return (
-                            <li key={i}>
-                                <Link to="/">{item.title}</Link>
-                                <span>{item.updateTime.split(' ')[0]}</span>
-                            </li>
-                        );
-                    })
-                }
-                {/* <li><a href="#" target="_blank">聚焦智能金融巴巴汇金服获中国金融服务行业十大领先品牌聚焦智能金融巴巴汇金服获中国金融服务行业十大领先品牌聚焦智能金融巴巴汇金服获中国金融服务行业十大领先品牌</a><span>2017-12-8</span></li>
-                                <li><a href="#" target="_blank">聚焦智能金融 巴巴汇金服获中国金融服务行业十大领先品牌</a><span>2017-5-8</span></li>
-                                <li><a href="#" target="_blank">聚焦智能金融 巴巴汇金服获中国金融服务行业十大领先品牌</a><span>2017-5-8</span></li>
-                                <li><a href="#" target="_blank">聚焦智能金融 巴巴汇金服获中国金融服务行业十大领先品牌</a><span>2017-5-8</span></li>
-                                <li><a href="#" target="_blank">聚焦智能金融 巴巴汇金服获中国金融服务行业十大领先品牌</a><span>2017-5-8</span></li>
-                                <li><a href="#" target="_blank">聚焦智能金融 巴巴汇金服获中国金融服务行业十大领先品牌</a><span>2017-5-8</span></li>
-                                <li><a href="#" target="_blank">聚焦智能金融 巴巴汇金服获中国金融服务行业十大领先品牌</a><span>2017-5-8</span></li>
-                                <li><a href="#" target="_blank">聚焦智能金融 巴巴汇金服获中国金融服务行业十大领先品牌</a><span>2017-5-8</span></li>
-                                <li><a href="#" target="_blank">聚焦智能金融 巴巴汇金服获中国金融服务行业十大领先品牌</a><span>2017-5-8</span></li>
-            <li><a href="#" target="_blank">聚焦智能金融 巴巴汇金服获中国金融服务行业十大领先品牌</a><span>2017-5-8</span></li> */}
-            </ul>
-
+            <div>
+                <div className="tabs__nav">
+                    <li className="tab tab--active">{tabName}</li>
+                </div>
+                <div className="tabs__content">
+                    <ul className="list">
+                        {
+                            content.list.map((item, i) => {
+                                return (
+                                    <li key={i}>
+                                        <Link to="/">{item.title}</Link>
+                                        <span>{item.updateTime.split(' ')[0]}</span>
+                                    </li>
+                                );
+                            })
+                        }
+                    </ul>
+                </div>
+                <Pagination config={
+                    {
+                        currentPage: content.pageNum,
+                        pageSize: content.pageSize,
+                        totalPage: content.pages,
+                        hidden: false,
+                        paging: (obj) => {
+                            console.log(obj);
+                            //this.loadData(obj.currentPage,obj.pageCount);
+                        }
+                    }
+                } ></Pagination>
+            </div>
         )
     }
 }
+
+    const mapStateToProps = (state) => {
+        return {
+            aboutContent: state.toJS().aboutContentReducer
+        }
+    }
+
+articalList = connect(
+    mapStateToProps
+)(articalList)
 
 
 export default articalList;
