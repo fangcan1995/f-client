853f6e9e564576f3d464953dbaeaedbef29b9151
guifeng <fengyunlingying@qq.com>
2018-06-26 04:17:32 +0800

    add

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,5 +1,5 @@
-//export const urls ='http://47.95.39.231:8020/web';
-export const urls ='http://172.16.7.3:8020/web';
-//export const urls_auth ='http://47.95.39.231:8020';
-export const urls_auth ='http://172.16.7.3:8020';
+export const urls ='http://47.95.39.231:8020/web';
+//export const urls ='http://172.16.7.3:8020/web';
+export const urls_auth ='http://47.95.39.231:8020';
+//export const urls_auth ='http://172.16.7.3:8020';
 
